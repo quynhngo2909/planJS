bd42caafac9b75b24f68653e9ee55ccfe988dff3
quynhngo2909 <dieuquynh105@gmail.com>
2023-08-09 17:19:13 +0700

    fix comment

--- Express.js/src/configs/mongodbConnection.js
+++ Express.js/src/configs/mongodbConnection.js
@@ -1,20 +1,5 @@
 const mongoose = require("mongoose");
 
-// require("dotenv").config();
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// const connectToMongoDB = async () => {
-//     try {
-//         await mongoose.connect(MONGODB_URI);
-//         console.log("Connected to DB");
-//     }
-//     catch (err) {
-//         console.log(err + "");
-//     }
-// };
-
-// module.exports = connectToMongoDB;
-
 module.exports = connectToMongoDB = (mongoDbURI) => {
     return mongoose.connect(mongoDbURI);
 }
